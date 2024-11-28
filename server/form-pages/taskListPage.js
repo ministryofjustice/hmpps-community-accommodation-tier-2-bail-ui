5b3da043e4248dd769bc883f462bfc8bd9912421
Daniel Liburd <dan.liburd@digital.justice.gov.uk>
2024-11-28 09:25:53 +0000

    Remove unused JS files

--- server/form-pages/taskListPage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* istanbul ignore file */
-Object.defineProperty(exports, "__esModule", { value: true });
-var TaskListPage = /** @class */ (function () {
-    function TaskListPage() {
-    }
-    return TaskListPage;
-}());
-exports.default = TaskListPage;
