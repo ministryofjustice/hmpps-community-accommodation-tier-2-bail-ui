8cff67304000cf7186673f70c48ce8ddd1d449d0
Patrick Fleming <patrick.fleming1@digital.justice.gov.uk>
2024-12-02 10:43:01 +0000

    Add views and Nunjucks setup
    Copied from CAS2 HDC repo

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -3,9 +3,37 @@ import path from 'path'
 import nunjucks from 'nunjucks'
 import express from 'express'
 import fs from 'fs'
-import { initialiseName } from './utils'
+import { ErrorMessages, PersonStatus } from '@approved-premises/ui'
+import { initialiseName, removeBlankSummaryListItems, stringToKebabCase, camelToKebabCase } from './utils'
 import config from '../config'
 import logger from '../../logger'
+import applicationPaths from '../paths/apply'
+import assessPaths from '../paths/assess'
+import reportPaths from '../paths/report'
+import staticPaths from '../paths/static'
+import {
+  documentSummaryListRows,
+  inProgressApplicationTableRows,
+  submittedApplicationTableRows,
+  assessmentsTableRows,
+  prisonDashboardTableRows,
+  arePreTaskListTasksIncomplete,
+} from './applicationUtils'
+import {
+  getApplicationTimelineEvents,
+  getSideNavLinksForApplication,
+  getSideNavLinksForDocument,
+} from './applications/utils'
+import { applicationStatusRadios, applicationStatusDetailOptions } from './assessUtils'
+import { checkYourAnswersSections, getApplicantDetails } from './checkYourAnswersUtils'
+import { DateFormats } from './dateUtils'
+import { dateFieldValues } from './formUtils'
+import * as OasysImportUtils from './oasysImportUtils'
+import { statusTag } from './personUtils'
+import * as TaskListUtils from './taskListUtils'
+import { pagination } from './pagination'
+import { formatLines } from './viewUtils'
+import * as PhaseBannerUtils from './phaseBannerUtils'
 
 export default function nunjucksSetup(app: express.Express): void {
   app.set('view engine', 'njk')
@@ -39,4 +67,54 @@ export default function nunjucksSetup(app: express.Express): void {
 
   njkEnv.addFilter('initialiseName', initialiseName)
   njkEnv.addFilter('assetMap', (url: string) => assetManifest[url] || url)
+
+  njkEnv.addGlobal('fetchContext', function fetchContext() {
+    return this.ctx
+  })
+
+  njkEnv.addGlobal('mergeObjects', (obj1: Record<string, unknown>, obj2: Record<string, unknown>) => {
+    return { ...obj1, ...obj2 }
+  })
+
+  njkEnv.addGlobal('OasysImportUtils', OasysImportUtils)
+
+  njkEnv.addGlobal('paths', { ...applicationPaths, ...assessPaths, ...reportPaths, ...staticPaths })
+  njkEnv.addGlobal('TaskListUtils', TaskListUtils)
+
+  njkEnv.addGlobal('inProgressApplicationTableRows', inProgressApplicationTableRows)
+  njkEnv.addGlobal('submittedApplicationTableRows', submittedApplicationTableRows)
+  njkEnv.addGlobal('assessmentsTableRows', assessmentsTableRows)
+  njkEnv.addGlobal('documentSummaryListRows', documentSummaryListRows)
+  njkEnv.addGlobal('prisonDashboardTableRows', prisonDashboardTableRows)
+
+  njkEnv.addGlobal('dateFieldValues', function sendContextToDateFieldValues(fieldName: string, errors: ErrorMessages) {
+    return dateFieldValues(fieldName, this.ctx, errors)
+  })
+  njkEnv.addGlobal('formatDate', DateFormats.isoDateToUIDate)
+
+  njkEnv.addGlobal('checkYourAnswersSections', checkYourAnswersSections)
+  njkEnv.addGlobal('getApplicantDetails', getApplicantDetails)
+
+  njkEnv.addGlobal('getApplicationTimelineEvents', getApplicationTimelineEvents)
+  njkEnv.addGlobal('applicationStatusRadios', applicationStatusRadios)
+  njkEnv.addGlobal('applicationStatusDetailOptions', applicationStatusDetailOptions)
+
+  njkEnv.addFilter('removeBlankSummaryListItems', removeBlankSummaryListItems)
+  njkEnv.addFilter('formatLines', formatLines)
+
+  const markAsSafe = (html: string): string => {
+    const safeFilter = njkEnv.getFilter('safe')
+    return safeFilter(html)
+  }
+
+  njkEnv.addGlobal('statusTag', (status: PersonStatus) => markAsSafe(statusTag(status)))
+  njkEnv.addGlobal('getSideNavLinksForDocument', getSideNavLinksForDocument)
+  njkEnv.addGlobal('getSideNavLinksForApplication', getSideNavLinksForApplication)
+  njkEnv.addGlobal('stringToKebabCase', stringToKebabCase)
+  njkEnv.addGlobal('camelToKebabCase', camelToKebabCase)
+
+  njkEnv.addGlobal('pagination', pagination)
+
+  njkEnv.addGlobal('arePreTaskListTasksIncomplete', arePreTaskListTasksIncomplete)
+  njkEnv.addGlobal('PhaseBannerUtils', PhaseBannerUtils)
 }
