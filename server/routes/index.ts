a53002cfa1b60b914350155c5a7bbbbdfc17cc5c
Patrick Fleming <patrick.fleming1@digital.justice.gov.uk>
2024-12-02 11:04:59 +0000

    Pass controllers into routes

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,22 +1,25 @@
-import { Router } from 'express'
+/* istanbul ignore file */
 
-import type { Services } from '../services'
+import { Router } from 'express'
 import { Controllers } from '../controllers'
 import paths from '../paths/apply'
+import { Services } from '../services'
 import applyRoutes from './apply'
 import assessRoutes from './assess'
 import reportRoutes from './report'
 import staticRoutes from './static'
-
+import { actions } from './utils'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(controllers: Controllers, services: Services): Router {
   const router = Router()
-  const { get, post } = actions(router)
+  const { get, post } = actions(router, services.auditService)
 
-  const { dashboardController, peopleController } = controllers
+  const { dashboardController } = controllers
 
-  get('/', dashboardController.index())
+  get('/', dashboardController.index(), { auditEvent: 'VIEW_DASHBOARD' })
+
+  const { peopleController } = controllers
 
   post(paths.applications.people.find.pattern, peopleController.find(), {
     auditEvent: 'FIND_APPLICATION_PERSON',
