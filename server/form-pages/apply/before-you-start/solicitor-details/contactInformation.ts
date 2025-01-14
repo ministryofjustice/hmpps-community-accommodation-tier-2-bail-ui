83b034268fb2d237d66b689edf76b747b0d43d44
Daniel Liburd <dan.liburd@digital.justice.gov.uk>
2025-01-14 16:30:08 +0000

    Introduce solicitor details task

---
+++ server/form-pages/apply/before-you-start/solicitor-details/contactInformation.ts
@@ -0,0 +1,78 @@
+import { TaskListErrors } from '@approved-premises/ui'
+import { Cas2Application as Application } from '@approved-premises/api'
+import { Page } from '../../../utils/decorators'
+import TaskListPage from '../../../taskListPage'
+import { nameOrPlaceholderCopy } from '../../../../utils/utils'
+import { getQuestions } from '../../../utils/questions'
+
+export type ContactInformationBody = {
+  name: string
+  legalFirmAndAddress: string
+  email: string
+  number: string
+}
+
+@Page({
+  name: 'contact-information',
+  bodyProperties: ['name', 'legalFirmAndAddress', 'email', 'number'],
+})
+export default class ContactInformation implements TaskListPage {
+  documentTitle = "Add solicitor's contact information"
+
+  personName = nameOrPlaceholderCopy(this.application.person)
+
+  title
+
+  questions = getQuestions(this.personName)['solicitor-details']['contact-information']
+
+  body: ContactInformationBody
+
+  constructor(
+    body: Partial<ContactInformationBody>,
+    private readonly application: Application,
+  ) {
+    this.body = body as ContactInformationBody
+    this.title = this.questions.contactInformation.question
+  }
+
+  previous() {
+    return 'taskList'
+  }
+
+  next() {
+    return ''
+  }
+
+  errors() {
+    const errors: TaskListErrors<this> = {}
+
+    if (!this.body.name) {
+      errors.name = "Enter the solicitor's full name"
+    }
+
+    if (!this.body.legalFirmAndAddress) {
+      errors.legalFirmAndAddress = "Enter the solicitor's legal firm and address"
+    }
+
+    if (!this.body.email) {
+      errors.email = "Enter the solicitor's email address"
+    }
+
+    if (!this.body.number) {
+      errors.number = "Enter the solicitor's contact number"
+    }
+
+    return errors
+  }
+
+  response(): Record<string, string> {
+    const response: Record<string, string> = {}
+
+    const answerData: ContactInformationBody = this.application.data['solicitor-details']['contact-information']
+
+    response['Solicitor contact information'] =
+      `Name: ${answerData.name}\r\nLegal firm address: ${answerData.legalFirmAndAddress}\r\nEmail: ${answerData.email}\r\nPhone number: ${answerData.number}\r\n`
+
+    return response
+  }
+}
