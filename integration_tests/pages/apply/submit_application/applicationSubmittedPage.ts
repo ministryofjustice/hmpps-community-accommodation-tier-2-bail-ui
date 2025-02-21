c47a8ada0e6e0f3bc99dad55b04c2d0dead49deb
Patrick Fleming <patrick.fleming1@digital.justice.gov.uk>
2025-02-21 15:51:02 +0000

    Update integration tests with new confirm submission designs

--- integration_tests/pages/apply/submit_application/applicationSubmittedPage.ts
+++ integration_tests/pages/apply/submit_application/applicationSubmittedPage.ts
@@ -1,28 +1,16 @@
 import { Cas2v2Application as Application, FullPerson } from '@approved-premises/api'
 import Page from '../../page'
-import * as PhaseBannerUtils from '../../../../server/utils/phaseBannerUtils'
 
 export default class ApplicationSubmittedPage extends Page {
   constructor(private readonly application: Application) {
     const person = application.person as FullPerson
-    super('Application complete', person.name)
+    super('Application submitted', person.name)
   }
 
   shouldShowApplicationDetails(): void {
     const person = this.application.person as FullPerson
 
-    cy.get('.govuk-list--spaced').within(() => {
-      cy.get('li').contains(this.application.id)
-      cy.get('li').contains(person.name)
-      cy.get('li').contains(person.nomsNumber)
-    })
-  }
-
-  shouldShowLinkToFeedbackSurvey(): void {
-    cy.contains('a', 'Share your feedback for this service').should(
-      'have.attr',
-      'href',
-      PhaseBannerUtils.feedbackSurveyUrl,
-    )
+    cy.get('h2').contains(this.application.id)
+    cy.get('h2').contains(person.name)
   }
 }
