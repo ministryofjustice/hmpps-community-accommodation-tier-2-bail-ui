a2e66d9986895ddcb112fba80932b5823cbeb1e8
Daniel Liburd <dan.liburd@digital.justice.gov.uk>
2025-02-26 17:03:55 +0000

    Remove existing risk pages
    We'll be adding new pages to this task, so remove existing pages in
    preparation.
    
    The Acct page is kept for 2 reasons:
    
    1. Each task requires at least one page to present for the task list to
       render
    
    2. We will be reusing the Acct page in the new design.

--- server/form-pages/apply/risks-and-needs/risk-information/index.ts
+++ server/form-pages/apply/risks-and-needs/risk-information/index.ts
@@ -1,16 +1,11 @@
 /* istanbul ignore file */
+import Acct from './acct'
 
 import { Task } from '../../../utils/decorators'
-import Acct from './acct'
-import AcctData from './custom-forms/acctData'
-import AdditionalInformation from './additionalInformation'
-import CurrentRisk from './currentRisk'
-import HistoricalRisk from './historicalRisk'
-import Vulnerability from './vulnerability'
 
 @Task({
   name: 'Add information about risks to the applicant and others',
   slug: 'risk-information',
-  pages: [Vulnerability, CurrentRisk, HistoricalRisk, AcctData, Acct, AdditionalInformation],
+  pages: [Acct],
 })
 export default class RiskToSelf {}
