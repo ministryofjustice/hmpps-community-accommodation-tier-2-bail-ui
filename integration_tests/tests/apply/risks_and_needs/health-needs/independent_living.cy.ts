13c83058ab4d8441d3722d4115cf8eb5d95fc343
Patrick Fleming <patrick.fleming1@digital.justice.gov.uk>
2025-03-05 10:00:24 +0000

    Add integration tests for missing health needs pages
    These pages were missed previously, so basic integration tests are added for both of them.

--- integration_tests/tests/apply/risks_and_needs/health-needs/independent_living.cy.ts
+++ integration_tests/tests/apply/risks_and_needs/health-needs/independent_living.cy.ts
@@ -1,27 +1,27 @@
-//  Feature: Referrer completes 'Health needs information' page
+//  Feature: Referrer completes 'Independent living' page
 //    So that I can complete the "Health needs" task
 //    As a referrer
-//    I want to confirm that I've understood the information on that page
+//    I want to complete the independent living page
 //
 //  Scenario: Follows link from task list
 //    Given there is a section with a task
 //    And an application exists
 //    And I am logged in
 //
-//  Scenario: view health needs information page
-//    When I visit the health needs information page
-//    Then I see the health needs information page
+//  Scenario: view Independent living page
+//    When I visit the Independent living page
+//    Then I see the Independent living page
 //
 //  Scenario: continues to next page in "health needs" task
 //    When I continue to the next task/page
 //    Then I should be on the substance misuse page
 
-import SubstanceMisusePage from '../../../../pages/apply/risks_and_needs/health-needs/substanceMisusePage'
 import Page from '../../../../pages/page'
 import HealthNeedsInformationPage from '../../../../pages/apply/risks_and_needs/health-needs/healthNeedsInformationPage'
 import { personFactory, applicationFactory } from '../../../../../server/testutils/factories/index'
+import IndependentLivingPage from '../../../../pages/apply/risks_and_needs/health-needs/independentLivingPage'
 
-context('Visit health needs information page', () => {
+context('Visit Independent living page', () => {
   const person = personFactory.build({ name: 'Roger Smith' })
 
   beforeEach(function test() {
@@ -51,22 +51,22 @@ context('Visit health needs information page', () => {
     cy.signIn()
   })
 
-  //  Scenario: view health needs information page
+  //  Scenario: view Independent living page
   it('shows the page', function test() {
-    //  When I visit the health needs information page
-    HealthNeedsInformationPage.visit(this.application)
+    //  When I visit the Independent living page
+    IndependentLivingPage.visit(this.application)
 
-    //  Then I see the health needs information page
-    Page.verifyOnPage(HealthNeedsInformationPage, this.application)
+    //  Then I see the Independent living page
+    Page.verifyOnPage(IndependentLivingPage, this.application)
   })
 
   //  Scenario: continues to next page in "health needs" task
   it('continues to the next page', function test() {
     //  When I continue to the next task/page
-    const page = HealthNeedsInformationPage.visit(this.application)
-    page.clickContinue()
+    const page = IndependentLivingPage.visit(this.application)
+    page.clickSubmit()
 
-    //  Then I should be on the substance misuse page
-    Page.verifyOnPage(SubstanceMisusePage, this.application)
+    //  Then I should be on the health needs information page
+    Page.verifyOnPage(HealthNeedsInformationPage, this.application)
   })
 })
