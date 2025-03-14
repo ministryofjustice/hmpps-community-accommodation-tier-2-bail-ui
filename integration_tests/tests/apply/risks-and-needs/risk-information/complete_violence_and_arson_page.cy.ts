b4a8bf905f239e2f434271095cb11f477dff4db4
Daniel Liburd <dan.liburd@digital.justice.gov.uk>
2025-03-14 11:24:27 +0000

    Update integration test for violence and arson page

--- integration_tests/tests/apply/risks-and-needs/risk-information/complete_violence_and_arson_page.cy.ts
+++ integration_tests/tests/apply/risks-and-needs/risk-information/complete_violence_and_arson_page.cy.ts
@@ -1,9 +1,25 @@
-//  Feature: complete violence and arson page
+//  Feature: Referrer completes "risk information: violence and arson" page
+//    So that I can complete the "risk information" task
+//    As a referrer
+//    I want to complete the 'violence and arson' page
 //
+//  Background:
+//    Given an application exists
+//    And I am logged in
+//    And I am on the violence and arson page
+//
+//  Scenario: view violence and arson questions
+//    Then I see the "violence and arson" page
+//
+//  Scenario: complete page and navigate to next page in risk information task
+//    When I complete the violence and arson page
+//    And I continue to the next task / page
+//    Then I see the "living in the community" page
 
 import ViolenceAndArsonPage from '../../../../pages/apply/risks-and-needs/risk-information/violenceAndArsonPage'
 import Page from '../../../../pages/page'
 import { personFactory, applicationFactory } from '../../../../../server/testutils/factories/index'
+import LivingInTheCommunityPage from '../../../../pages/apply/risks-and-needs/risk-information/livingInTheCommunityPage'
 
 context('Complete "Violence and arson" page', () => {
   const person = personFactory.build({ name: 'Roger Smith' })
@@ -39,8 +55,25 @@ context('Complete "Violence and arson" page', () => {
     cy.visit('applications/abc123/tasks/risk-information/pages/violence-and-arson')
   })
 
-  it('exists', function test() {
-    // Given I am on the Violence and arson page
+  //  Scenario: view violence and arson questions
+  //    Then I see the "violence and arson" page
+  it('presents the violence and arson page', function test() {
     Page.verifyOnPage(ViolenceAndArsonPage, this.application)
+  })
+
+  //  Scenario: complete page and navigate to next page in risk information task
+  //    When I complete the violence and arson page
+  //    And I continue to the next task / page
+  //    Then I see the "living in the community" page
+  it('navigates to the next page (living in the community)', function test() {
+    ViolenceAndArsonPage.visit(this.application)
+    const page = new ViolenceAndArsonPage(this.application)
+
+    page.describePastConvictions()
+    page.describeCurrentConcerns()
+
+    page.clickSubmit()
+
+    Page.verifyOnPage(LivingInTheCommunityPage, this.application)
   })
 })
