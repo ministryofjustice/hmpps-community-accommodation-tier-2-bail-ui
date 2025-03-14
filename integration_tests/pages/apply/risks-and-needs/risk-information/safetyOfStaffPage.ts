ca0708b06e8f19398650d848bd6dc2848a64b6c7
Patrick Fleming <patrick.fleming1@digital.justice.gov.uk>
2025-03-14 11:18:10 +0000

    Update integration tests with new safety of staff questions

--- integration_tests/pages/apply/risks-and-needs/risk-information/safetyOfStaffPage.ts
+++ integration_tests/pages/apply/risks-and-needs/risk-information/safetyOfStaffPage.ts
@@ -3,6 +3,25 @@ import ApplyPage from '../../applyPage'
 
 export default class SafetyOfStaffPage extends ApplyPage {
   constructor(private readonly application: Application) {
-    super('Safety of staff', application, 'risk-information', 'safety-of-staff')
+    super('Concerns related to the safety of staff', application, 'risk-information', 'safety-of-staff')
+  }
+
+  enterPastRiskDetails(): void {
+    this.checkRadioByNameAndValue('pastRiskToStaff', 'yes')
+    this.getTextInputByIdAndEnterDetails('pastRiskToStaffDetail', 'some details')
+  }
+
+  enterCurrentRiskDetails(): void {
+    this.checkRadioByNameAndValue('currentConcerns', 'yes')
+    this.getTextInputByIdAndEnterDetails('currentConcernsDetail', 'some further details')
+  }
+
+  hasGuidanceText = (): void => {
+    cy.get('.guidance').contains('Consider concerns towards')
+    cy.get('.guidance').contains('support and housing staff')
+    cy.get('.guidance').contains('probation workers')
+    cy.get('.guidance').contains('accommodation staff')
+    cy.get('.guidance').contains('police staff')
+    cy.get('.guidance').contains('custody staff')
   }
 }
--- integration_tests/tests/apply/risks-and-needs/risk-information/complete_safety_of_staff_page.cy.ts
+++ integration_tests/tests/apply/risks-and-needs/risk-information/complete_safety_of_staff_page.cy.ts
@@ -1,9 +1,26 @@
-//  Feature: complete Safety of staff page
+//  Feature: Referrer completes 'Safety of staff' page
+//    So that I can complete the "Risk" task
+//    As a referrer
+//    I want to complete the 'Safety of staff' page
 //
+//  Background:
+//    Given an application exists
+//    And I am logged in
+//    And I am on the 'Safety of staff' page
+//
+//  Scenario: view Safety of staff page
+//    Then I see the "Safety of staff" page
+//    And I see guidance text
+//
+//  Scenario: complete page and navigate to next page in risk task
+//    When I complete the Safety of staff page
+//    And I continue to the next task / page
+//    Then I see the "additional concerns" page
 
 import SafetyOfStaffPage from '../../../../pages/apply/risks-and-needs/risk-information/safetyOfStaffPage'
 import Page from '../../../../pages/page'
 import { personFactory, applicationFactory } from '../../../../../server/testutils/factories/index'
+import AdditionalConcernsPage from '../../../../pages/apply/risks-and-needs/risk-information/additionalConcernsPage'
 
 context('Complete "Safety of staff" page', () => {
   const person = personFactory.build({ name: 'Roger Smith' })
@@ -39,8 +56,26 @@ context('Complete "Safety of staff" page', () => {
     cy.visit('applications/abc123/tasks/risk-information/pages/safety-of-staff')
   })
 
-  it('exists', function test() {
-    // Given I am on the Safety of staff page
-    Page.verifyOnPage(SafetyOfStaffPage, this.application)
+  //  Scenario: view Safety of staff page
+  it('displays the page', function test() {
+    //  Then I see the "Safety of staff" page
+    const page = Page.verifyOnPage(SafetyOfStaffPage, this.application)
+
+    //  And I see guidance text
+    page.hasGuidanceText()
+  })
+
+  //  Scenario: complete page and navigate to next page in risk task
+  it('continues to the next page', function test() {
+    //  When I complete the Safety of staff page
+    const page = Page.verifyOnPage(SafetyOfStaffPage, this.application)
+    page.enterPastRiskDetails()
+    page.enterCurrentRiskDetails()
+
+    //  And I continue to the next task / page
+    page.clickSubmit()
+
+    //  Then I see the "additional concerns" page
+    Page.verifyOnPage(AdditionalConcernsPage, this.application)
   })
 })
