87ac2b236f5cb6fba22976200851dc5ebc88a277
Daniel Liburd <dan.liburd@digital.justice.gov.uk>
2025-03-21 15:26:33 +0000

    Add overrides to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,7 @@
 
 import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
-export default hmppsConfig()
+export default hmppsConfig({
+  extraIgnorePaths: ['e2e-tests/playwright-report'],
+  extraPathsAllowingDevDependencies: ['esbuild-configs/**'],
+})
