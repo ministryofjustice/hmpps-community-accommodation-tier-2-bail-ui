8c3a1035d877560e6c0e0e77e6067f506afc845d
Patrick Fleming <patrick.fleming1@digital.justice.gov.uk>
2025-04-01 12:02:03 +0100

    Add integration tests for new referrer location page

--- integration_tests/tests/apply/before_you_apply/referrer-details/location.cy.ts
+++ integration_tests/tests/apply/before_you_apply/referrer-details/location.cy.ts
@@ -2,26 +2,27 @@
 //  Feature: Referrer completes 'Referrer details' task
 //    So that I can complete the 'Referrer details' task
 //    As a referrer
-//    I want to answer questions on the 'Contact number' page
+//    I want to answer questions on the 'Location' page
 //
 //  Background:
 //    Given I am logged in
-//    And I'm on the 'Contact number' page
+//    And I'm on the 'Location' page
 //
-//  Scenario: view 'Contact Number' page
-//    Then I see the "Contact Number" page
+//  Scenario: view 'Location' page
+//    Then I see the "Location" page
 //
-//  Scenario: navigate to next page in task
-//    When I complete the "Contact number" page
+//  Scenario: navigate to task list on completion of task
+//    When I complete the "Location" page
 //    And I continue to the next task / page
-//    Then I am taken to the location page
+//    Then I am taken back to the task list
+//    And I see that the referrer details task is complete
 
 import Page from '../../../../pages/page'
 import { personFactory, applicationFactory } from '../../../../../server/testutils/factories/index'
-import ContactNumberPage from '../../../../pages/apply/before_you_apply/referrer_details/contactNumberPage'
 import LocationPage from '../../../../pages/apply/before_you_apply/referrer_details/locationPage'
+import TaskListPage from '../../../../pages/apply/taskListPage'
 
-context('Complete "Contact number" page in "Referrer details" task', () => {
+context('Complete "Location" page in "Referrer details" task', () => {
   const person = personFactory.build({ name: 'Roger Smith' })
 
   beforeEach(function test() {
@@ -47,17 +48,17 @@ context('Complete "Contact number" page in "Referrer details" task', () => {
     cy.task('stubApplicationGet', { application: this.application })
     cy.task('stubApplicationUpdate', { application: this.application })
 
-    //  And I'm on the 'Contact number' page
-    ContactNumberPage.visit(this.application)
+    //  And I'm on the 'Location' page
+    LocationPage.visit(this.application)
   })
 
-  //  Scenario: view 'Contact Number' page
+  //  Scenario: view 'Location' page
   it('displays page', function test() {
-    // Then I see the "Contact Number" page
-    Page.verifyOnPage(ContactNumberPage, this.application)
+    // Then I see the "Location" page
+    Page.verifyOnPage(LocationPage, this.application)
   })
 
-  //  Scenario: navigate to next page in the task
+  //  Scenario: navigate to task list on completion of task
   it('navigates to the task list', function test() {
     // So that the status of the task will be complete we set application.data
     // to the full set
@@ -69,14 +70,17 @@ context('Complete "Contact number" page in "Referrer details" task', () => {
       cy.task('stubApplicationGet', { application: answered })
     })
 
-    // When I complete the "Contact number" page
-    const page = Page.verifyOnPage(ContactNumberPage, this.application)
+    // When I complete the "Location" page
+    const page = Page.verifyOnPage(LocationPage, this.application)
     page.completeForm()
 
     // And I continue to the next task / page
     page.clickSubmit()
 
-    // Then I am taken to the location page
-    Page.verifyOnPage(LocationPage, this.application)
+    // Then I am taken back to the task list
+    const taskListPage = Page.verifyOnPage(TaskListPage)
+
+    // And I see that the referrer details task is complete
+    taskListPage.shouldShowTaskStatus('referrer-details', 'Completed')
   })
 })
