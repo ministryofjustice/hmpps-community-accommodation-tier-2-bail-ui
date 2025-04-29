e43a47b5470ff90d135716e254c308090ae5fe8f
Patrick Fleming <patrick.fleming1@digital.justice.gov.uk>
2025-04-29 14:46:40 +0100

    Fix gang affiliation page title in E2E tests

--- e2e-tests/steps/areaAndFundingSection.ts
+++ e2e-tests/steps/areaAndFundingSection.ts
@@ -75,7 +75,7 @@ async function completeExclusionZonesPage(page: Page, name: string) {
 }
 
 async function completeGangAffiliations(page: Page, name: string) {
-  const exclusionZonesPage = await ApplyPage.initialize(page, `Does ${name} have any gang affiliations?`)
+  const exclusionZonesPage = await ApplyPage.initialize(page, `Gang affiliation details for ${name}`)
 
   await exclusionZonesPage.checkRadioInGroup('any gang affiliations', 'Yes')
   await exclusionZonesPage.fillField('details of the gang', 'Gang name')
