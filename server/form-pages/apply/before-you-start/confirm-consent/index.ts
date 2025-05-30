b5101570a58ba0097d6befb7961ab53463c3df31
Patrick Fleming <patrick.fleming1@digital.justice.gov.uk>
2025-05-30 14:49:47 +0100

    Rename consent task

--- server/form-pages/apply/before-you-start/confirm-consent/index.ts
+++ server/form-pages/apply/before-you-start/confirm-consent/index.ts
@@ -5,7 +5,7 @@ import ConfirmConsentPage from './confirmConsent'
 import ConsentRefused from './consentRefused'
 
 @Task({
-  name: 'Confirm consent to share information',
+  name: 'Confirm consent to apply',
   slug: 'confirm-consent',
   pages: [ConfirmConsentPage, ConsentRefused],
 })
