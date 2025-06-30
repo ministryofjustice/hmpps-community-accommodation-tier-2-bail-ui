854f67be78ccd04b1ac40688619e5c7a4d00bdf9
Patrick Fleming <patrick.fleming1@digital.justice.gov.uk>
2025-06-30 15:13:12 +0100

    Update accessibility statement

--- server/utils/phaseBannerUtils.test.ts
+++ server/utils/phaseBannerUtils.test.ts
@@ -4,7 +4,7 @@ describe('PhaseBannerUtils', () => {
   it('returns the phase banner content', () => {
     const feedbackSurveyUrl =
       'https://forms.office.com/Pages/ResponsePage.aspx?id=KEeHxuZx_kGp4S6MNndq2NyAjsSBbUdAmHEQmZLKk-lURTlTUUtBMFNMN0I3RDdQR0lIUlNZN0xBRi4u'
-    const content = `This is a new service. <a class="govuk-link" rel="noreferrer noopener" target="_blank" href="${feedbackSurveyUrl}">Complete our feedback form</a> (opens in new tab) or <a class="govuk-link" href="mailto:cas2-bail-digital-support@justice.gov.uk">email us</a> (cas2-bail-digital-support@justice.gov.uk) to report a bug`
+    const content = `This is a new service. <a class="govuk-link" rel="noreferrer noopener" target="_blank" href="${feedbackSurveyUrl}">Complete our feedback form</a> (opens in new tab) or <a class="govuk-link" href="mailto:cas2support@digital.justice.gov.uk">email us</a> (cas2support@digital.justice.gov.uk) to report a bug`
 
     expect(getContent()).toEqual(content)
   })
