70ea45895e0fdc2e7f17e56351aa2f6dd013ebeb
davidatkinsuk <22135634+davidatkinsuk@users.noreply.github.com>
2026-08-03 09:37:47 +0000

    Updating hmpps-approved-premises-api models from OpenAPI specification

--- server/@types/shared/models/Cas2SubmittedApplicationSummary.ts
+++ server/@types/shared/models/Cas2SubmittedApplicationSummary.ts
@@ -3,9 +3,11 @@
 /* tslint:disable */
 /* eslint-disable */
 import type { ApplicationOrigin } from './ApplicationOrigin';
+import type { Cas2CohortDto } from './Cas2CohortDto';
 export type Cas2SubmittedApplicationSummary = {
     applicationOrigin?: ApplicationOrigin;
     bailHearingDate?: string;
+    cohort?: Cas2CohortDto;
     createdAt: string;
     createdByUserId: string;
     crn: string;
